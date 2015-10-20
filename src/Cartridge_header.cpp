36225d000f9a67ffb633413cdca0d7f3a26ba3cf
DeiNile <okristensen@albany.edu>
2015-10-20 19:33:55 -0400

    Implemented a header class for the cartridge. Made appropriate changes to Makefile

---
+++ src/Cartridge_header.cpp
@@ -0,0 +1,127 @@
+#include "headers/Cartridge_header.hpp"
+
+Header::Header(uint8_t bytes[])
+{
+	int i = 0;
+	for (; i < NES_NAME_LENGTH; i++) {
+		nes_name += bytes[i];
+	}
+	PRG_size_16KB = bytes[i++];
+	CHR_size_8KB  = bytes[i++];
+	flag_6        = bytes[i++];
+	flag_7        = bytes[i++];
+	PRG_size_8KB  = bytes[i++];
+	flag_9        = bytes[i++];
+}
+
+int Header::get_CHR_size_8KB() 
+{
+	return CHR_size_8KB;
+}
+
+int Header::get_PRG_size_8KB()
+{
+	return PRG_size_8KB;	
+}
+
+int Header::get_PRG_size_16KB()
+{
+	return PRG_size_16KB;
+}
+
+uint8_t Header::get_flag_6()
+{
+	return flag_6;
+}
+
+uint8_t Header::get_flag_7()
+{
+	return flag_7;
+}
+
+uint8_t Header::get_flag_9()
+{
+	return flag_9;
+}
+
+bool Header::valid_header()
+{
+	if (nes_name == NES_NAME) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool Header::is_vertical()
+{
+	return arrangement_mirroring;
+}
+
+bool Header::is_horizontal()
+{
+	return !arrangement_mirroring;
+}
+
+bool Header::is_four_screen_VRAM()
+{
+	return four_screen_VRAM;
+}
+
+bool Header::is_battery_present()
+{
+	return battery_packed_PRG_RAM;
+}
+
+bool Header::is_trainer_present()
+{
+	return trainer;
+}
+
+bool Header::is_unisystem()
+{
+	return unisystem;
+}
+
+bool Header::is_playchoice()
+{
+	return playchoice;
+}
+
+bool Header::is_NTSC()
+{
+	return tv_system;
+}
+
+bool Header::is_PAL()
+{
+	return !tv_system;
+}
+
+
+void Header::update_flags()
+{
+	uint8_t temp = flag_6;
+	if ((temp & MIRRORING_INDEX_BIT) == 0) {
+		arrangement_mirroring = (temp & 1);
+	} else {
+		four_screen_VRAM = true;
+	}
+	temp >>= 1;
+	battery_packed_PRG_RAM = (temp & 1);
+	temp >>= 1;
+	trainer = (temp & 1);
+
+	temp = flag_7;
+	unisystem = (temp & 1);
+	temp >>= 1;
+	playchoice = (temp & 1);
+
+	tv_system = (flag_9 & 1);
+	if ((flag_7 & NES2_FORMAT_MASK) == NES2_FORMAT_MASK) {
+		nes2 = true;
+	}
+
+	nybble = (flag_7 & UPPER_NYBBLE_MASK) | (flag_6 >> NYBBLE_LENGTH);
+}
+
---
+++ src/headers/Cartridge_header.hpp
@@ -0,0 +1,65 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdint.h>
+#include <iostream>
+
+#define NES_NAME_LENGTH 4
+#define HEADER_LENGTH 16
+#define ZERO_FIELD_INDEX 11
+#define MIRRORING_INDEX_BIT 0x3
+#define NES2_FORMAT_MASK 0x4
+#define NYBBLE_LENGTH 4
+#define UPPER_NYBBLE_MASK 0xF0
+#define NES_NAME "NES\x1A"
+#define TRAINER_SIZE 512
+
+
+class Header {
+public:
+	Header(uint8_t[]);
+	int get_CHR_size_8KB();
+	int get_PRG_size_16KB();
+	int get_PRG_size_8KB();
+	uint8_t get_flag_6();
+	uint8_t get_flag_7();
+	uint8_t get_flag_9();
+	bool valid_header();
+	bool is_vertical();
+	bool is_horizontal();
+	bool is_four_screen_VRAM();
+	bool is_battery_present();
+	bool is_trainer_present();
+	bool is_unisystem();
+	bool is_playchoice();
+	bool is_NTSC();
+	bool is_PAL();
+protected:
+private:
+	int PRG_size_16KB;
+	int CHR_size_8KB;
+	int PRG_size_8KB;
+	uint8_t flag_6;
+	uint8_t flag_7;
+	uint8_t flag_9;
+	uint8_t nybble;
+	std::string nes_name;
+	bool nes2;
+
+	// Flags 6
+	bool arrangement_mirroring; // 0 vertical, 1 horizontal
+	bool four_screen_VRAM;
+	bool battery_packed_PRG_RAM;
+	bool trainer;
+
+	// Flags 7
+	bool unisystem;
+	bool playchoice;
+
+	// Flags 9
+	bool tv_system; // 0 NTSC, 1 PAL
+
+	void update_flags();
+};
+
+#endif
