8603405596f475e35a19f469f3e3c9ff929ef094
DeiNile <okristensen@albany.edu>
2015-12-29 08:47:44 -0500

    Added an outline for the PPU rendering logic

--- src/Console.cpp
+++ src/Console.cpp
@@ -1,4 +1,9 @@
 #include "headers/Console.hpp"
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
 
 Console::Console()
 {
@@ -30,6 +35,14 @@ void Console::load_cartridge(Cartridge *cartridge)
 
 void Console::power_on()
 {
+	// struct sigaction;
+	// struct sigIntHandler;
+
+	// sigIntHandler.sa_handler = power_off;
+	// sigemptyset(&sigIntHandler.sa_mask);
+	// sigIntHandler.sa_flags = 0;
+	// sigaction(SIGINT, &sigIntHandler, NULL);
+
 	if (cart == NULL) {
 		std::cerr << "ERROR: No Cartridge was loaded." << std::endl << 
 			"Exiting..." << std::endl;
@@ -40,6 +53,16 @@ void Console::power_on()
 		cpu->fetch_and_execute();
 	}
 }
+
+// void Console::power_off(int signal)
+// {
+// 	std::cout << "Exiting..." << std::endl;
+// 	delete ppu;
+// 	delete cpu;
+// 	delete cart;
+
+// 	exit(1);
+// }
 
 void Console::reset()
 {
